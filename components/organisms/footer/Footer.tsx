aba3e50981062e537a27f050909b00a48b19411b
Cristina Jung <cristijung@gmail.com>
2024-05-23 16:16:08 -0300

    icons

---
+++ components/organisms/footer/Footer.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Footer() {
+    return(
+        <>
+        Componente para footer
+        </>
+    )
+}
---
+++ components/organisms/header/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return(
+        <>
+        Componente header principal
+        </>
+    )
+}
