c82829ffcca6e7e3c9c38d8bc0e75f90bb1b1db7
RafaelR4mos <rafaelramos0401@gmail.com>
2024-05-23 17:22:29 -0300

    fix: btn style transition property

--- components/atoms/default-btn/DefaultBtn.tsx
+++ components/atoms/default-btn/DefaultBtn.tsx
@@ -16,7 +16,7 @@ export default function DefaultBtn({
         <button
           className="py-4 px-5 text-base font-normal
          bg-brand-main text-white h-[3rem] min-w-[11.75rem] 
-         flex items-center justify-center
+         flex items-center justify-center transition-all duration-300
          hover:bg-brand-dark hover:rounded-lg
          active:bg-brand-darker active:rounded-lg
          disabled:bg-brand-light disabled:cursor-not-allowed disabled:rounded-none disabled:text-gray-light
@@ -32,7 +32,7 @@ export default function DefaultBtn({
         <button
           className="py-4 px-5 text-base font-normal bg-white
            text-brand-darker h-[3rem] min-w-[11.75rem] 
-           flex items-center justify-center
+           flex items-center justify-center duration-300
            hover:bg-gray-light hover:rounded-lg
            active:bg-gray-darker active:rounded-lg active:text-white
            disabled:bg-gray-dark disabled:cursor-not-allowed disabled:rounded-none disabled:text-gray-main 
@@ -48,7 +48,7 @@ export default function DefaultBtn({
         <button
           className="py-4 px-5 text-base font-normal text-brand-main
         flex items-center justify-center
-        gap-2
+        gap-2 duration-300
         hover:text-brand-dark
         active:text-brand-darker
         disabled:text-brand-light disabled:cursor-not-allowed disabled:opacity-75
