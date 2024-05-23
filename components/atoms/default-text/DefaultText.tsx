e74bd05223fe12af61c4bc8b6ce96a54017d81db
Cristina Jung <cristijung@gmail.com>
2024-05-23 17:27:30 -0300

    adjust textlight and textdark

--- components/atoms/default-text/DefaultText.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function DefaultText() {
-    return(
-        <>
-        Texto padrão
-        </>
-    )
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 
+import DefaultTextDark from "@/components/atoms/default-text/DefaultTextDark";
+import DefaultTextLight from "@/components/atoms/default-text/DefaultTextLight";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ 
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export default function Home() {
   return (
-   <>
-   Start
-
-  
+   <>   
+   <DefaultTextDark text="Teste de estilização" />
+   <DefaultTextLight text="Teste de estilização" />
    </>
   );
 }
