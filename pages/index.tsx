96b982c006c0c0920bdd778e0f90628e57987310
RafaelR4mos <rafaelramos0401@gmail.com>
2024-05-23 16:24:13 -0300

    wip button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 
+import DefaultBtn from "@/components/atoms/default-btn/DefaultBtn";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -6,7 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
    <>
-   Start
+    <DefaultBtn>
+       primary
+    </DefaultBtn>
+
+    <DefaultBtn variant="secondary">
+       secondary
+    </DefaultBtn>
    </>
   );
 }
