61ff7d3f89eb72671f544bc6c3a0616e505bf1a3
RafaelR4mos <rafaelramos0401@gmail.com>
2024-05-23 17:13:31 -0300

    default button component done

--- components/atoms/default-btn/DefaultBtn.tsx
+++ components/atoms/default-btn/DefaultBtn.tsx
@@ -34,11 +34,26 @@ export default function DefaultBtn({
            text-brand-darker h-[3rem] min-w-[11.75rem] 
            flex items-center justify-center
            hover:bg-gray-light hover:rounded-lg
-           active:bg-gray-darker active:rounded-lg text-white
-           disabled:bg-brand-light disabled:cursor-not-allowed disabled:rounded-none
-           focus:outline-2 focus:outline-brand-dark
+           active:bg-gray-darker active:rounded-lg active:text-white
+           disabled:bg-gray-dark disabled:cursor-not-allowed disabled:rounded-none disabled:text-gray-main 
+           focus:outline-2 focus:outline-brand-darker focus:bg-white
            "
-           
+          {...rest}
+        >
+          {children}
+        </button>
+      )}
+
+      {variant === 'text-button' && (
+        <button
+          className="py-4 px-5 text-base font-normal text-brand-main
+        flex items-center justify-center
+        gap-2
+        hover:text-brand-dark
+        active:text-brand-darker
+        disabled:text-brand-light disabled:cursor-not-allowed disabled:opacity-75
+        focus:border-1 focus:border-brand-dark
+        "
           {...rest}
         >
           {children}
