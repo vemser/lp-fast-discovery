3beaff90b81f5db03aab4b123c429e7e0dda3cbe
Cristina Jung <cristijung@gmail.com>
2024-05-27 18:01:03 -0300

    card 91% loading

--- components/molecules/card-lp/CardsLp.tsx
+++ components/molecules/card-lp/CardsLp.tsx
@@ -1,3 +1,4 @@
+import DefaultTextGeral from "@/components/atoms/default-text/DefaultTextGeral";
 import Image from "next/image";
 import React from "react";
 
@@ -12,8 +13,8 @@ export default function CardsLp({ icon, alt, title, children }: CardLpProps) {
   return (
     <div className="max-w-md bg-white shadow-md rounded-lg p-4 flex flex-col items-start md:w-[410px] md:h-[260px]">
     <Image src={icon} alt={alt} width={48} height={48} className="mb-2" />
-    <h2 className="text-lg font-semibold mb-2 py-8">{title}</h2>
-    <div className="text-left w-full">{children}</div>
+    <h2 className="text-lg font-semibold mb-2 py-8">{title}</h2> {/**Ver como fazer esta parte, aproveitar o component ou deixar a tag */}
+    <DefaultTextGeral variant="dark">{children}</DefaultTextGeral>    
   </div>
   );
 }
