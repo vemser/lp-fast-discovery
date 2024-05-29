261652c466713b670e8943d422289d8d712af374
Cristina Jung <cristijung@gmail.com>
2024-05-29 14:03:10 -0300

    ready card

--- components/molecules/card-lp/CardsLp.tsx
+++ components/molecules/card-lp/CardsLp.tsx
@@ -11,10 +11,12 @@ interface CardLpProps {
 
 export default function CardsLp({ icon, alt, title, children }: CardLpProps) {
   return (
-    <div className="max-w-md bg-white shadow-md rounded-lg p-4 flex flex-col items-start md:w-[410px] md:h-[260px]">
-    <Image src={icon} alt={alt} width={48} height={48} className="mb-2" />
-    <h2 className="text-lg font-semibold mb-2 py-8">{title}</h2> {/**Ver como fazer esta parte, aproveitar o component ou deixar a tag */}
-    <DefaultTextGeral variant="dark">{children}</DefaultTextGeral>    
+    <div className="max-w-md bg-white shadow-lg rounded-lg p-4 flex flex-col items-start md:w-[410px] md:h-[260px]">
+      <div className="ml-4">
+    <Image src={icon} alt={alt} width={48} height={48} className="mb-2 pt-6" />
+    <h2 className="text-lg font-semibold mb-2 py-16 pb-0">{title}</h2> 
+    <DefaultTextGeral variant="dark">{children}</DefaultTextGeral>
+    </div>    
   </div>
   );
 }
